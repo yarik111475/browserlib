b3d189f4f2564cf17823b7a536ca15e8b104eeb2
yarik111475 <yarik111475@mail.ru>
2022-03-04 18:20:06 +0300

    change browserlibest project settings

--- browserlibTest/browserlibtest.cpp
+++ browserlibTest/browserlibtest.cpp
@@ -1,6 +1,8 @@
 #include "browserlibtest.h"
+#include "qttreepropertybrowser.h"
 
 browserlibTest::browserlibTest(QWidget *parent): QMainWindow(parent)
 {
-
+	QtTreePropertyBrowser* pTreeBrowser = new QtTreePropertyBrowser(this);
+	setCentralWidget(pTreeBrowser);
 }
